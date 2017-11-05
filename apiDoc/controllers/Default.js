c70790f78f98f7f310b3c26c0b02dccc764b5801
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2017-11-05 19:20:39 +0100

    Correct contact

--- apiDoc/controllers/Default.js
+++ apiDoc/controllers/Default.js
@@ -1,11 +1,7 @@
-'use strict';
+'use strict'
 
-var url = require('url');
-
-
-var Default = require('./DefaultService');
-
+var Default = require('./DefaultService')
 
 module.exports.registerPOST = function registerPOST (req, res, next) {
-  Default.registerPOST(req.swagger.params, res, next);
-};
+  Default.registerPOST(req.swagger.params, res, next)
+}
--- apiDoc/controllers/DefaultService.js
+++ apiDoc/controllers/DefaultService.js
@@ -1,23 +1,20 @@
-'use strict';
+'use strict'
 
-exports.registerPOST = function(args, res, next) {
+exports.registerPOST = function (args, res, next) {
   /**
    * parameters expected in the args:
   * body (User)
   **/
-    var examples = {};
+  var examples = {}
   examples['application/json'] = {
-  "success" : true,
-  "message" : "aeiou",
-  "accessToken" : "aeiou"
-};
-  if(Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+    'success': true,
+    'message': 'aeiou',
+    'accessToken': 'aeiou'
   }
-  else {
-    res.end();
+  if (Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2))
+  } else {
+    res.end()
   }
-  
 }
-
