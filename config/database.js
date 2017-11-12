d482fa75b12a7cae637a8d54cb2d1b1ef398efd2
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2017-11-12 19:41:16 +0100

    Add feedback/tests/default env
    /register now responds with "missing parameters" when not sending both email and password. Wrote matching tests to scheck responce on incorrect post to /register. When mysql user/pass is not found defaults root and empty string are used.

--- config/database.js
+++ config/database.js
@@ -1,9 +1,10 @@
 const Sequelize = require('sequelize')
-
+const user = process.env.MYSQL_USER || 'root'
+const pass = process.env.MYSQL_PASS || ''
 const database = 'karneval'
 const databaseURL = 'localhost'
 
-const dbc = new Sequelize(database, process.env.MYSQL_USER, process.env.MYSQL_PASS, {
+const dbc = new Sequelize(database, user, pass, {
   host: databaseURL,
   dialect: 'mysql'
 })
