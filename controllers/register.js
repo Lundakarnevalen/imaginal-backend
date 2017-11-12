e49cafdcb27c1e330cfc922e4554483e5c198025
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2017-11-12 19:41:16 +0100

    Add feedback/tests/default env
    /register now responds with "missing parameters" when not sending both email and password. Wrote matching tests to scheck responce on incorrect post to /register. When mysql user/pass is not found defaults root and empty string are used.

--- controllers/register.js
+++ controllers/register.js
@@ -1,27 +1,43 @@
 const sequelize = require('../config/database.js').dbc
 const users = require('../models/users')
 const bcrypt = require('bcrypt')
+const validateUser = require('../utils/validator').validateUser
 
-let registerUser = function (req, res) {
-  // 10 rounds used for salt, needs to be tested and optimized
-  bcrypt.hash(req.body.password, 10, function (err, hash) {
-    if (err) {
-      throw err
-    }
-    sequelize.sync()
-    .then(() => users.User.create({
-      email: req.body.email,
-      password: hash,
-      token: 'temporary token'
-    }))
-    .then(user => {
-      let resp = {}
-      resp.success = true
-      resp.message = 'You are now registered'
-      resp.token = user.token
-      res.send(resp)
+const registerUser = function (req, res) {
+  const success = (msg) => {
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+      if (err) {
+        throw err
+      }
+      sequelize.sync()
+        .then(() => users.User.create({
+          email: req.body.email,
+          password: hash,
+          token: 'temporary token'
+        }))
+        .then(user => {
+          res.json({
+            success: true,
+            message: 'You are now registered',
+            token: user.token
+          })
+        })
     })
-  })
+  }
+
+  const fail = (message) => {
+    res.status(400).json({
+      success: false,
+      message
+    })
+  }
+
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  }
+
+  validateUser(user, success, fail)
 }
 
 module.exports = {
