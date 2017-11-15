0c284aa8fcf081d90f211a8e1c973685be0c3709
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2017-11-15 23:12:48 +0100

    logic moved

--- app.js
+++ app.js
@@ -17,35 +17,7 @@ app.post('/register', Register.registerUser)
 /** LOGIN */
 
 // Validera innan vi ger till passport?
-app.post('/login/email', function (req, res, next) {
-  if (!req.body.email || !req.body.password) {
-    res.json({message: 'missing parameters'})
-  }
-  passport.authenticate('local',
-    function (err, user, info) {
-      if (err) {
-        return res.json({
-          message: info
-        })
-      } else if (!user) {
-        return res.json({
-          message: info
-        })
-      }
-      req.logIn(user, function (err) {
-        if (err) {
-          return res.json({
-            message: 'Login Failure!'
-          })
-        }
-        return res.json({
-          success: true,
-          message: 'Successfully logged in',
-          accessToken: req.user.token
-        })
-      })
-    })(req, res, next)
-})
+app.post('/login/email', login.login)
 
 /** AUTHENTICATE TOKENS */
 app.all(/(\/)?api\/.*/,
--- controllers/login.js
+++ controllers/login.js
@@ -1,11 +1,35 @@
 'use strict'
+const passport = require('passport')
+require('../config/passport')(passport)
 
-let login = function (req, res) {
-  res.send({
-    success: true,
-    message: 'Successfully logged in',
-    accessToken: req.user.token
-  })
+let login = function (req, res, next) {
+  if (!req.body.email || !req.body.password) {
+    res.json({message: 'Missing parameters'})
+  }
+  passport.authenticate('local',
+    function (err, user, info) {
+      if (err) {
+        return res.json({
+          message: info
+        })
+      } else if (!user) {
+        return res.json({
+          message: info
+        })
+      }
+      req.logIn(user, function (err) {
+        if (err) {
+          return res.json({
+            message: 'Login failure!'
+          })
+        }
+        return res.json({
+          success: true,
+          message: 'Successfully logged in',
+          accessToken: req.user.token
+        })
+      })
+    })(req, res, next)
 }
 
 module.exports = {
