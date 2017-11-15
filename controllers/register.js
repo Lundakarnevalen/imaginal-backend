5406ed1dfc8417e578be7f778f9ffdb404197d03
Nafrali <pt.niklas.karlsson@telia.com>
2017-11-15 17:58:29 +0100

    No longer possible to register with same email twice

--- controllers/register.js
+++ controllers/register.js
@@ -2,15 +2,30 @@ const sequelize = require('../config/database.js').dbc
 const users = require('../models/users')
 
 let registerUser = function (req, res) {
+  users.User.findOne({
+    where: {email: req.body.email}
+  }).then((user) => {
+    if (user !== null) {
+      let resp = {}
+      resp.success = false
+      resp.message = 'User already registered'
+      res.send(resp)
+    } else {
+      createUser(req.body.email, req.body.password, res)
+    }
+  })
   // 10 rounds used for salt, needs to be tested and optimized
+}
+
+let createUser = function(email, password, res) {
   sequelize.sync()
     .then(() => users.User.create({
-      email: req.body.email,
+      email: email,
       token: 'temporary token'
     }))
     .then(user => {
       let resp = {}
-      users.setNewPassword(user, req.body.password)
+      users.setNewPassword(user, password)
       resp.success = true
       resp.message = 'You are now registered'
       resp.token = user.token
