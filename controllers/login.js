505d21a56a7b8758e4472c81a4a4dcc0453d6766
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2017-11-19 16:32:09 +0100

    feedback

--- controllers/login.js
+++ controllers/login.js
@@ -3,20 +3,23 @@ const passport = require('passport')
 
 const loginByEmail = function (req, res, next) {
   if (!req.body.email || !req.body.password) {
-    res.status(422).json({message: 'Missing parameters'})
+    res.status(422).json({success: false, message: 'Missing parameters'})
   }
   passport.authenticate('local',
     function (err, user, info) {
       if (err) {
         return res.status(500).send(info)
       } else if (!user) {
-        return res.status(401).send(info)
+        return res.status(401).json({
+          success: false,
+          message: info
+        })
       }
       req.logIn(user, function (err) {
         if (err) {
           return res.json({
             success: false,
-            message: 'Login failure!'
+            message: 'Incorrect login credentials.'
           })
         }
         return res.json({
