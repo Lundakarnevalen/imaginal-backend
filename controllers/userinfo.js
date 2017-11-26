c834604c462e9a53951910ec18b59c72194f4fa9
MaxLindblom <max.g.lindblom@gmail.com>
2017-11-26 16:00:37 +0100

    Added body check for user info

--- controllers/userinfo.js
+++ controllers/userinfo.js
@@ -1,13 +1,13 @@
 'use strict'
 
 let setUserInfo = function (req, res) {
-  req.user.name = req.body.name
-  req.user.phoneNumber = req.body.phoneNumber
-  req.user.address = req.body.address
-  req.user.postNumber = req.body.postNumber
-  req.user.city = req.body.city
-  req.user.careOf = req.body.careOf
-  req.user.personalNumber = req.body.personalNumber
+  if (req.body.name) req.user.name = req.body.name
+  if (req.body.phoneNumber) req.user.phoneNumber = req.body.phoneNumber
+  if (req.body.address) req.user.address = req.body.address
+  if (req.body.postNumber) req.user.postNumber = req.body.postNumber
+  if (req.body.city) req.user.city = req.body.city
+  if (req.body.careOf) req.user.careOf = req.body.careOf
+  if (req.body.personalNumber) req.user.personalNumber = req.body.personalNumber
   req.user.save().then(() => {
     res.json({
       success: true,
