a01e51c9c2b00bd6338ccbb0f6c19dd3cca50015
Joakim Almgren <joakim.oa@gmail.com>
2017-12-03 16:28:21 +0100

    standard js fixes

--- models/userrole.js
+++ models/userrole.js
@@ -1,19 +1,19 @@
-'use strict';
+'use strict'
 const Sequelize = require('sequelize')
 const dbc = require('../config/database')
 
 const UserRole = dbc.define('UserRole', {
-    UserId: Sequelize.INTEGER,
-    RoleId: Sequelize.INTEGER
-  }, {
-    classMethods: {
-      associate: function(models) {
+  UserId: Sequelize.INTEGER,
+  RoleId: Sequelize.INTEGER
+}, {
+  classMethods: {
+    associate: function (models) {
         // associations can be defined here
-        models.UserRole.hasOne(User, {as: UserId, foreignKey: 'UserId'})
-        models.UserRole.hasOne(Role, {as: RoleId, foreignKey: 'RoleId'})
-      }
+      models.UserRole.hasOne(User, {as: UserId, foreignKey: 'UserId'})
+      models.UserRole.hasOne(Role, {as: RoleId, foreignKey: 'RoleId'})
     }
-  })
+  }
+})
 
 module.exports = {
   UserRole
