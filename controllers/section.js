02e98bb6e8d595f273f3567b1db34ab38268fef0
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2017-12-03 22:24:29 +0100

    post/get contract

---
+++ controllers/section.js
@@ -0,0 +1,47 @@
+const SectionPriorities = require('../models/sectionPriority')
+
+const setSectionPriorities = function (req, res, next) {
+  if (!req.body.sectionPriorities || !Array.isArray(req.body.sectionPriorities)) {
+    res.status(422).json({success: false, message: 'Missing parameters'})
+  }
+
+  SectionPriorities.setSectionPriorities(req.user, req.body.sectionPriorities, function (err, success) {
+    if (err) {
+      return res.status(500).json({
+        success,
+        message: 'Priorities were not set'
+      })
+    }
+    if (success) {
+      return res.json({
+        success,
+        message: 'Priorities set successfully'
+      })
+    } else {
+      return res.status(403).json({
+        success,
+        message: 'Priorities can no longer be set'
+      })
+    }
+  })
+}
+
+const getSectionPriorities = function (req, res, next) {
+  SectionPriorities.getSectionPriorities(req.user, function (err, sectionPriorities) {
+    if (err) {
+      res.status(500).json({
+        success: false,
+        message: 'Failed to get priorities'
+      })
+    }
+    res.json({
+      success: true,
+      sectionPriorities
+    })
+  })
+}
+
+module.exports = {
+  setSectionPriorities,
+  getSectionPriorities
+}
--- models/sectionpriority.js
+++ models/sectionpriority.js
@@ -13,6 +13,46 @@ const SectionPriority = dbc.define('SectionPriority', {
   prio: Sequelize.INTEGER
 })
 
+const setSectionPriorities = function (user, sectionPriorities, done) {
+  const lastDate = new Date(2020, 0, 0, 0, 0, 0, 0) // new Date(year, month, day, hours, minutes, seconds, milliseconds)
+  if (Date.now() > lastDate.getTime()) {
+    return done(null, false)
+  }
+
+  if (isValidPrioArray(sectionPriorities)) {
+    return done('Invalid array!', false)
+  }
+
+  SectionPriority.destroy({
+    where: {
+      user_id: user.id
+    }
+  }).then(() => sectionPriorities.forEach((currentValue, index) => SectionPriority.create({user_id: user.id, section: currentValue, prio: index})))
+  return done(null, true)
+}
+
+const getSectionPriorities = function (user, done) {
+  SectionPriority.findAll({
+    where: {
+      user_id: user.id
+    }
+  }).then(prios => done(null, prios.reduce(flatten, [])))
+}
+
+const isValidPrioArray = function (array, callback) {
+  if (Array.isArray(array)) {
+    return (new Set(array)).size !== array.length
+  }
+  return false
+}
+
+const flatten = function (acc, cVal, cInd, arr){
+  acc.push(cVal.section)
+  return acc
+}
+
 module.exports = {
-  SectionPriority
+  SectionPriority,
+  getSectionPriorities,
+  setSectionPriorities
 }
