bd2df9eaedc94e0eacb8fdf551cdd65c27492242
Edvin Havic <edvinhavic@gmail.com>
2018-01-22 05:34:58 +0100

    bugfix

--- controllers/users.js
+++ controllers/users.js
@@ -69,6 +69,13 @@ const getById = async (req, res) => {
     ],
   })
 
+  if (!user) {
+    return res.status(400).json({
+      success: false,
+      message: 'No such user',
+    })
+  }
+
   user = user.toJSON()
   user.checkedIn = !!user.Checkin
 
@@ -109,29 +116,36 @@ const setUserInfo = function(req, res) {
       if (req.body.postNumber) req.user.postNumber = req.body.postNumber
       if (req.body.city) req.user.city = req.body.city
       if (req.body.careOf) req.user.careOf = req.body.careOf
-      if (req.body.personalNumber)
+      if (req.body.personalNumber) {
         req.user.personalNumber = req.body.personalNumber
+      }
 
       if (req.body.language) entry.language = req.body.language
-      if (req.body.driversLicense)
+      if (req.body.driversLicense) {
         entry.driversLicense = req.body.driversLicense
-      if (req.body.foodPreference)
+      }
+      if (req.body.foodPreference) {
         entry.foodPreference = req.body.foodPreference
+      }
       if (req.body.disability) entry.disability = req.body.disability
       if (req.body.audition) entry.audition = req.body.audition
       if (req.body.talent) entry.talent = req.body.talent
-      if (req.body.entertainmentCategory)
+      if (req.body.entertainmentCategory) {
         entry.entertainmentCategory = req.body.entertainmentCategory
+      }
       if (req.body.corps) entry.corps = req.body.corps
-      if (req.body.startOfStudies)
+      if (req.body.startOfStudies) {
         entry.startOfStudies = req.body.startOfStudies
-      if (req.body.pastInvolvement)
+      }
+      if (req.body.pastInvolvement) {
         entry.pastInvolvement = req.body.pastInvolvement
+      }
       if (req.body.groupLeader) entry.groupLeader = req.body.groupLeader
       if (req.body.interests) entry.interests = req.body.interests
       if (req.body.misc) entry.misc = req.body.misc
-      if (req.body.plenipotentiary)
+      if (req.body.plenipotentiary) {
         entry.plenipotentiary = req.body.plenipotentiary
+      }
       user
         .save()
         .then(() => entry.save())
