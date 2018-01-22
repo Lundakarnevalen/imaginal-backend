01e6e55a7895cdf7088b1ff20cb59b8f98ade451
Edvin Havic <edvinhavic@gmail.com>
2018-01-22 06:04:14 +0100

    Refactored setUserInfo, it was too painful for my eyes and gave me sleeping issues

--- controllers/users.js
+++ controllers/users.js
@@ -85,77 +85,71 @@ const getById = async (req, res) => {
   })
 }
 
-const setUserInfo = function(req, res) {
-  UserRoles.hasRole(req.user, 'administrator')
-    .then(isadmin => {
-      return new Promise((resolve, reject) => {
-        if (!isadmin) {
-          if (req.params.email !== req.user.email) {
-            return res.status(401).json({
-              success: false,
-              message: 'Permission denied',
-            })
-          }
-          resolve(req.user)
-        } else {
-          users.User.findOne({
-            where: { email: req.params.email },
-            include: [{ model: users.KarnevalistInfo }],
-          }).then(dbuser => {
-            resolve(dbuser)
-          })
-        }
-      })
-    })
-    .then(user => {
-      let entry = user.KarnevalistInfo
-      if (req.body.firstName) req.user.firstName = req.body.firstName
-      if (req.body.lastName) req.user.lastName = req.body.lastName
-      if (req.body.phoneNumber) req.user.phoneNumber = req.body.phoneNumber
-      if (req.body.address) req.user.address = req.body.address
-      if (req.body.postNumber) req.user.postNumber = req.body.postNumber
-      if (req.body.city) req.user.city = req.body.city
-      if (req.body.careOf) req.user.careOf = req.body.careOf
-      if (req.body.personalNumber) {
-        req.user.personalNumber = req.body.personalNumber
-      }
+const setUserInfo = async (req, res) => {
+  const email = req.params.email
+  const isAdmin = await UserRoles.hasRole(req.user, 'administrator')
 
-      if (req.body.language) entry.language = req.body.language
-      if (req.body.driversLicense) {
-        entry.driversLicense = req.body.driversLicense
-      }
-      if (req.body.foodPreference) {
-        entry.foodPreference = req.body.foodPreference
-      }
-      if (req.body.disability) entry.disability = req.body.disability
-      if (req.body.audition) entry.audition = req.body.audition
-      if (req.body.talent) entry.talent = req.body.talent
-      if (req.body.entertainmentCategory) {
-        entry.entertainmentCategory = req.body.entertainmentCategory
-      }
-      if (req.body.corps) entry.corps = req.body.corps
-      if (req.body.startOfStudies) {
-        entry.startOfStudies = req.body.startOfStudies
-      }
-      if (req.body.pastInvolvement) {
-        entry.pastInvolvement = req.body.pastInvolvement
-      }
-      if (req.body.groupLeader) entry.groupLeader = req.body.groupLeader
-      if (req.body.interests) entry.interests = req.body.interests
-      if (req.body.misc) entry.misc = req.body.misc
-      if (req.body.plenipotentiary) {
-        entry.plenipotentiary = req.body.plenipotentiary
-      }
-      user
-        .save()
-        .then(() => entry.save())
-        .then(() => {
-          return res.json({
-            success: true,
-            message: 'User info updated',
-          })
-        })
+  if (!isAdmin && email !== req.user.email) {
+    return res.status(401).json({
+      success: false,
+      message: 'Permission denied',
     })
+  }
+
+  const user = await users.User.findOne({
+    where: { email },
+    include: [{ model: users.KarnevalistInfo }],
+  })
+
+  if (!user) {
+    return res.status(400).json({
+      success: false,
+      message: 'No such user',
+    })
+  }
+
+  // All editable user fields
+  const fields = [
+    'firstName',
+    'lastName',
+    'phoneNumber',
+    'address',
+    'postNumber',
+    'city',
+    'careOf',
+    'personalNumber',
+  ]
+
+  // All editable karnevalistInfo fields
+  const entryFields = [
+    'language',
+    'driversLicense',
+    'foodPreference',
+    'disability',
+    'audition',
+    'talent',
+    'entertainmentCategory',
+    'corps',
+    'startOfStudies',
+    'pastInvolvement',
+    'groupLeader',
+    'interests',
+    'misc',
+    'plenipotentiary',
+  ]
+
+  const entry = user.KarnevalistInfo
+
+  fields.forEach(key => (req.user[key] = req.body[key] || req.user[key]))
+  entryFields.forEach(key => (entry[key] = req.body[key] || entry[key]))
+
+  await user.save()
+  await entry.save()
+
+  return res.json({
+    success: true,
+    message: 'User info updated',
+  })
 }
 
 module.exports = {
