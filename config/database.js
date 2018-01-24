a9e469cb343c21d63f6ccc069f12a16024b26d8e
jullekungfu <julian.krone@hotmail.com>
2018-01-24 21:08:52 +0100

    more

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 const user = process.env.MYSQL_USER || 'root'
 const pass = process.env.MYSQL_PASS || ''
-const database = 'karneval'
+const database = process.env.RDS_DB_NAME || 'karneval'
 const databaseURL = process.env.MYSQL_HOST || 'localhost'
 
 module.exports = new Sequelize(database, user, pass, {
