8de08976b9b7bd3d27e891a93362b3a0ca3cf0ed
jullekungfu <julian.krone@hotmail.com>
2018-01-24 20:12:42 +0100

    add aws conf

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 const user = process.env.MYSQL_USER || 'root'
 const pass = process.env.MYSQL_PASS || ''
 const database = 'karneval'
-const databaseURL = 'localhost'
+const databaseURL = process.env.MYSQL_HOST || 'localhost'
 
 module.exports = new Sequelize(database, user, pass, {
   host: databaseURL,
