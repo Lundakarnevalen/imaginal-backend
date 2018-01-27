17a3b6bd986fbbe4b8e1ec0d1f9bc63f959bfcff
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2018-01-27 16:29:11 +0100

    script

---
+++ sync.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const login = require('./controllers/login')
+const register = require('./controllers/register')
+const forgotPassword = require('./controllers/forgotpassword')
+const passport = require('passport')
+require('./config/passport')(passport)
+const role = require('./controllers/role')
+const section = require('./controllers/section')
+const checkin = require('./controllers/checkin')
+const users = require('./controllers/users')
+
+app.use(bodyParser.json())
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./config/database')
+  .sync()
+  .then(() => {
+    process.exit()
+  })
