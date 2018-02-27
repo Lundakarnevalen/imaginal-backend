ec25faf5779d064f7989aec9393c1705553ecf0e
Nafrali <pt.niklas.karlsson@telia.com>
2018-02-27 18:00:26 +0100

    Now logs admin in

--- seeders/20180201152701-admin-and-apple-seed.js.js
+++ seeders/20180201152701-admin-and-apple-seed.js.js
@@ -12,7 +12,7 @@ module.exports = {
       city: 'saknas',
       careOf: 'saknas',
       personalNumber: 'saknas',
-      password: '$2a$10$HEbL5HaUXdD3ZnUDhQnMYueuW3n4v92aPs4imN.FlRD/on4DI/55e',
+      password: '$2a$10$gb05lhNSTK2XZQbYmhJSk.TFRYvgBjNIeMTEvMfSPRQYlKRhdcJbe',
       token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGx1bmRha2FybmV2YWxlbi5zZSIsImlhdCI6MTUxNzg2MzM0MH0.sTXMU6V2elo9kxTe-W8bd9KAT0XfR8_qFlZdBD9emMM',
       createdAt: new Date().toISOString().substr(0, 10),
       updatedAt: new Date().toISOString().substr(0, 10)
