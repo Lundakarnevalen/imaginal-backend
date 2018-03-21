03fd74b8f0a5a24572d096185c14cfeda4c75081
osk419 <osk419@gmail.com>
2018-03-21 19:26:51 +0100

    refactored add storage

--- controllers/storagelocation.js
+++ controllers/storagelocation.js
@@ -3,25 +3,24 @@
 const storageLocations = require('../models/storagelocation')
 
 const addStorageLocation = async (req, res) => {
-  storageLocations.StorageLocation.findAll()
-    .then((locations) => {
-      locations.forEach((location) => {
-        if (location.storageName === req.body.storageName) {
-          return res.json({
-            success: false,
-            message: 'Location already exists'
-          })
-        }
-      })
-      storageLocations.StorageLocation.create({
-        storageName: req.body.storageName
-      }).then(() => {
-        res.json({
-          success: true,
-          message: 'Storage Location added'
-        })
-      })
+  const locations = await storageLocations.StorageLocation.findAll()
+  const existingLocation = await locations.filter(location => location.storageName === req.body.storageName)
+  if (existingLocation.length > 0) {
+    res.json({
+      success: false,
+      message: 'Storage Location already exists'
     })
+  }
+  else {
+    //Check if create succeeds.
+    await storageLocations.StorageLocation.create({
+      storageName: req.body.storageName
+    })
+    res.json({
+      success: true,
+      message: 'Storage Location added'
+    })
+  }
 }
 
 const getStorageLocations = async (req, res) => {
@@ -33,22 +32,22 @@ const getStorageLocations = async (req, res) => {
 }
 
 //skeleton for editing storage location
-const editStorageLocation = function (req, res) {
-  storageLocations.StorageLocation.findAll()
-    .then((locations) => {
-      locations.forEach((location) => {
-        if (location.storageName === req.body.storageName) {
-          return res.json({
-            success: true,
-            message: 'Storage Location edited'
-          })
-        }
-      })
-      res.json({
-        success: false,
-        message: 'No such location exists'
-      })
+const editStorageLocation = async (req, res) => {
+  const locations = await storageLocations.StorageLocation.findAll()
+  const existingLocation = await locations.filter(location => location.storageName === req.body.storageName)
+  if (existingLocation.length == 0) {
+    res.json({
+      success: false,
+      message: 'Storage Location does not exist'
     })
+  }
+  else {
+    //Add code for editing
+    res.json({
+      success: true,
+      message: 'Storage Location edited'
+    })
+  }
 }
 
 module.exports = {
