07f393dd770c24d312bef2f9b043a6256104abdb
osk419 <osk419@gmail.com>
2018-03-24 17:15:11 +0100

    add update and getbyID and refactoring

--- controllers/storagelocation.js
+++ controllers/storagelocation.js
@@ -4,15 +4,14 @@ const storageLocations = require('../models/storagelocation')
 
 const addStorageLocation = async (req, res) => {
   const locations = await storageLocations.StorageLocation.findAll()
-  const existingLocation = await locations.filter(location => location.storageName === req.body.storageName)
-  if (existingLocation.length > 0) {
+  const existingLocation = await locations.find(location => location.storageName === req.body.storageName)
+  if (existingLocation) {
     res.json({
       success: false,
       message: 'Storage Location already exists'
     })
-  }
-  else {
-    //Check if create succeeds.
+  } else {
+    // Check if create succeeds.
     await storageLocations.StorageLocation.create({
       storageName: req.body.storageName
     })
@@ -31,27 +30,46 @@ const getStorageLocations = async (req, res) => {
   })
 }
 
-//skeleton for editing storage location
-const editStorageLocation = async (req, res) => {
+const getByID = async (req, res) => { 
   const locations = await storageLocations.StorageLocation.findAll()
-  const existingLocation = await locations.filter(location => location.storageName === req.body.storageName)
-  if (existingLocation.length == 0) {
+  const theLocation = await locations.find(location => location.id === req.body.id)
+  if (theLocation) {
+    res.json({
+      success: true,
+      location: theLocation
+    })
+  } else {
+    res.json({
+      success: false,
+      message: 'No such ID'
+    })
+  }
+}
+
+const updateStorageLocation = async (req, res) => {
+  const locations = await storageLocations.StorageLocation.findAll()
+  const theLocation = await locations.find(location => location.id === req.body.id)
+  if (!theLocation) {
     res.json({
       success: false,
       message: 'Storage Location does not exist'
     })
-  }
-  else {
-    //Add code for editing
+  } else {
+    if(req.body.storageName)
+      theLocation.storageName = req.body.storageName
+    //add code for updating
+    //should we return the replaced fields?
+    await theLocation.save()
     res.json({
       success: true,
-      message: 'Storage Location edited'
+      message: 'Storage Location updated'
     })
   }
 }
 
 module.exports = {
   addStorageLocation,
-  //editStorageLocation,
-  getStorageLocations
+  updateStorageLocation,
+  getStorageLocations,
+  getByID
 }
