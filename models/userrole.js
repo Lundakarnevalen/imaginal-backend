ddb496196cff5d695776b935d5a1c58bf7df632a
Martin Johansson <martin.johansson1993@gmail.com>
2018-03-29 17:05:22 +0200

    fixed spelling

--- models/userrole.js
+++ models/userrole.js
@@ -46,7 +46,7 @@ const hasRole = function (user, role) { // (user, role) returns a boolean-promis
 
 const ADMIN = 'administrator'
 const CUSTOMER = 'warehouse customer'
-const WOKER = 'warehouse worker'
+const WOKRER = 'warehouse worker'
 const MANAGER = 'warehouse manager'
 
 module.exports = {
